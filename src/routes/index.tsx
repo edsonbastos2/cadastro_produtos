605b7c225fb7f16023e74219b4ff6adeeab7bcb0
Edson Bastos <abastos@uolin.com>
2023-02-04 14:57:23 -0300

    refactor: adicionado eslint

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
-import {Routes, Route, Navigate} from 'react-router-dom'
-import {Button} from '@mui/material'
-import { useAppThemeContext } from '../shared/contexts'
+import {Routes, Route, Navigate} from 'react-router-dom';
+import {Button} from '@mui/material';
+import { useAppThemeContext } from '../shared/contexts';
 
 
 export const AppRoutes = () => {
-    const {toggleTheme} = useAppThemeContext()
-    return(
-        <Routes>
-            <Route path='/pagina-inicial' element={<Button onClick={toggleTheme} variant='contained' color='primary'>Teste</Button>} />
-            <Route path='*' element={<Navigate to="/pagina-inicial"/>}/>
-        </Routes>
-    )
-}
+  const {toggleTheme} = useAppThemeContext();
+  return(
+    <Routes>
+      <Route path='/pagina-inicial' element={<Button onClick={toggleTheme} variant='contained' color='primary'>Teste</Button>} />
+      <Route path='*' element={<Navigate to="/pagina-inicial"/>}/>
+    </Routes>
+  );
+};
--- src/shared/contexts/themeContext.tsx
+++ src/shared/contexts/themeContext.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useCallback, useContext, useMemo, useState } from "react";
-import {ThemeProvider } from '@mui/material'
-import {Box} from '@mui/system'
-import { DarkTheme, LightTheme } from "../themes";
+import React, { createContext, ReactNode, useCallback, useContext, useMemo, useState } from 'react';
+import {ThemeProvider } from '@mui/material';
+import {Box} from '@mui/system';
+import { DarkTheme, LightTheme } from '../themes';
 
 interface IThemeContextData {
     themeName: 'light' | 'dark';
@@ -12,33 +12,33 @@ interface ThemeProviderProps {
     children: ReactNode
 }
 
-const ThemeContext = createContext({} as IThemeContextData)
+const ThemeContext = createContext({} as IThemeContextData);
 
 export const useAppThemeContext = () => {
-    return useContext(ThemeContext)
-}
+  return useContext(ThemeContext);
+};
 
 export const AppThemeProvider: React.FC<ThemeProviderProps> = ({children}) => {
 
-    const [themeName, setThemeName] = useState<'light' | 'dark'>('light')
+  const [themeName, setThemeName] = useState<'light' | 'dark'>('light');
 
-    const toggleTheme = useCallback(() =>{
-        setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light')
-    },[])
+  const toggleTheme = useCallback(() =>{
+    setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light');
+  },[]);
 
-    const theme = useMemo(() => {
-        if(themeName === 'light') return LightTheme
+  const theme = useMemo(() => {
+    if(themeName === 'light') return LightTheme;
 
-        return DarkTheme
-    },[themeName])
+    return DarkTheme;
+  },[themeName]);
 
-    return(
-        <ThemeContext.Provider value={{themeName, toggleTheme}}>
-            <ThemeProvider theme={theme}>
-                <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
-                    {children}
-                </Box>
-            </ThemeProvider>
-        </ThemeContext.Provider>
-    )
-}
+  return(
+    <ThemeContext.Provider value={{themeName, toggleTheme}}>
+      <ThemeProvider theme={theme}>
+        <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
+          {children}
+        </Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
